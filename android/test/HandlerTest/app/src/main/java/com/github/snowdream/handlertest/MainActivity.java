f7a7ca63a5023215ab3ece41fb3b81473d128319
snowdream <yanghui1986527@gmail.com>
2017-06-07 16:25:28 +0800

    Add：handlerTest

---
+++ android/test/HandlerTest/app/src/main/java/com/github/snowdream/handlertest/MainActivity.java
@@ -0,0 +1,67 @@
+package com.github.snowdream.handlertest;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.badoo.mobile.util.WeakHandler;
+
+public class MainActivity extends AppCompatActivity {
+    //private WeakHandler mHandler = new WeakHandler(); // We still need at least one hard reference to WeakHandler
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ImageView textView = (ImageView)findViewById(R.id.content);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setVisibility(View.INVISIBLE);
+                    }
+                },200000);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
